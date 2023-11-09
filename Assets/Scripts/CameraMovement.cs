27b02900dfe52316525ee7e2402cbccc1cb41c5c
leodaluz <daluz003@csusm.edu>
2023-11-08 21:46:17 -0800

    Camera Bounds

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -6,6 +6,11 @@ public class CameraMovement : MonoBehaviour
 {
     public Camera gameCamera;
     public float cameraMovementSpeed = 5;
+    public float MinimumXValue = -5;
+    public float MaximumXValue = 5;
+    public float MinimumZValue = -5;
+    public float MaximumZValue = 5;
+    public float pSpeed;
 
     private void Start()
     {
@@ -13,7 +18,7 @@ public class CameraMovement : MonoBehaviour
     }
     public void MoveCamera(Vector3 inputVector)
     {
-        var movementVector = Quaternion.Euler(0,30,0) * inputVector;
+        var movementVector = Quaternion.Euler(0, 30, 0) * inputVector;
         gameCamera.transform.position += movementVector * Time.deltaTime * cameraMovementSpeed;
     }
     public void CameraScroll()
@@ -25,6 +30,37 @@ public class CameraMovement : MonoBehaviour
         else if (Input.GetAxis("Mouse ScrollWheel") < 0f && gameCamera.orthographicSize < 10f) //ZoomOut
         {
             gameCamera.orthographicSize += .5f;
+        }
+    }
+    public void CameraBounds()
+    {
+        if (Input.GetKey("w"))
+        {
+            if (transform.position.z > MinimumZValue && transform.position.z < MaximumZValue)
+            {
+                transform.Translate(Vector3.forward * pSpeed * Time.deltaTime, Space.World);
+            }
+        }
+        if (Input.GetKey("a"))
+        {
+            if (transform.position.x > MinimumXValue && transform.position.x < MaximumXValue)
+            {
+                transform.Translate(Vector3.left * pSpeed * Time.deltaTime, Space.World);
+            }
+        }
+        if (Input.GetKey("d"))
+        {
+            if (transform.position.x > MinimumXValue && transform.position.x < MaximumXValue)
+            {
+                transform.Translate(Vector3.right * pSpeed * Time.deltaTime, Space.World);
+            }
+        }
+        if (Input.GetKey("s"))
+        {
+            if (transform.position.z > MinimumZValue && transform.position.z < MaximumZValue)
+            {
+                transform.Translate(Vector3.back * pSpeed * Time.deltaTime, Space.World);
+            }
         }
     }
 }
