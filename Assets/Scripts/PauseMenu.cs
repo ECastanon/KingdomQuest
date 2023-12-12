0466915fdab66b1368a44680f767ff2c88c07ff7
ECastanon <87353750+ECastanon@users.noreply.github.com>
2023-12-11 20:22:11 -0800

    Added Victory Message

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -5,12 +5,17 @@ using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
-    public GameObject PauseUI;
+    public GameObject PauseUI, WinUI;
+    private PeopleManager pm;
     public static bool paused = false;
-
+    public bool hasWon = false;
+    
     void Start()
     {
+        pm = GameObject.Find("PeopleManager").GetComponent<PeopleManager>();
+        WinUI = GameObject.Find("Win");
         PauseUI.SetActive(false);
+        WinUI.SetActive(false);
     }
     void Update()
     {
@@ -20,12 +25,23 @@ public class PauseMenu : MonoBehaviour
         }
         if (paused)
         {
+            HideSprites();
             PauseUI.SetActive(true);
             Time.timeScale = 0;
         } else if (!paused)
         {
+            ShowSprites();
             PauseUI.SetActive(false);
+            WinUI.SetActive(false);
             Time.timeScale = 1;
+        }
+
+        if(pm.population >= 50 && hasWon == false)
+        {
+            Resume();
+            hasWon = true;
+            WinUI.SetActive(true);
+            Time.timeScale = 0;
         }
     }
 
@@ -42,5 +58,19 @@ public class PauseMenu : MonoBehaviour
     public void Quit()
     {
         Application.Quit();
+    }
+    private void HideSprites()
+    {
+        Color tmp = new Color(1,1,1,0);
+        GameObject.Find("pngtree-cartoon-food-bread-whole-wheat-bread-png-image_423640").GetComponent<SpriteRenderer>().color = tmp;
+        GameObject.Find("istockphoto-1182472970-612x612").GetComponent<SpriteRenderer>().color = tmp;
+        GameObject.Find("smile").GetComponent<SpriteRenderer>().color = tmp;
+    }
+    private void ShowSprites()
+    {
+        Color tmp = new Color(1,1,1,1);
+        GameObject.Find("pngtree-cartoon-food-bread-whole-wheat-bread-png-image_423640").GetComponent<SpriteRenderer>().color = tmp;
+        GameObject.Find("istockphoto-1182472970-612x612").GetComponent<SpriteRenderer>().color = tmp;
+        GameObject.Find("smile").GetComponent<SpriteRenderer>().color = tmp;
     }
 }
